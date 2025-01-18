d21b7dae1c04abc6f91fb36294b6531c343b1309
antonlep <antonleppa@gmail.com>
2025-01-18 18:36:47 +0200

    add beer clubs

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,6 +3,9 @@ class Beer < ApplicationRecord
 
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
+  has_many :raters, through: :ratings, source: :user
+
+  validates :name, length: { minimum: 1 }
 
   def avg
     ratings.map(&:score).sum / ratings.count.to_f
